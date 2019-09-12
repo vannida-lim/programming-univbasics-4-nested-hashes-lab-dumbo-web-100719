7c49427c4ba1435c0f5f4450cb612431e5133115
Vannida Lim <vannida731@gmail.com>
2019-09-12 07:08:33 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -126,5 +126,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-dennis_new_info = 
+ dennis_new_info = "Assembly"
+ programmer_hash[:dennis_ritchie][:languages] = dennis_new_info
+ return programmer_hash
 end
